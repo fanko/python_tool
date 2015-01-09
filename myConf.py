b84e437e4e039de6b6eeec4fad1b93492e956be3
rong <rong@rong-qa06-vm.jr.rong360.com>
2015-01-09 19:08:18 +0800

    tag by qiwen

--- myConf.py
+++ myConf.py
@@ -3,7 +3,53 @@
 
 
 import sys
+import ConfigParser
+
+
+# class Config
+class Config:
+    def __init__(self, path):
+        self.path = path
+        self.cf = ConfigParser.ConfigParser()
+        self.cf.read(self.path)
+
+    def get(self, field, key):
+        result = None
+        try:
+            result = self.cf.get(field, key)
+        except:
+            pass
+        return result
+
+    def get_int(self, field, key):
+        result = None
+        try:
+            result = self.cf.getint(field, key)
+        except:
+            pass
+        return result
+
+    def get_float(self, field, key):
+        result = None
+        try:
+            result = self.cf.getfloat(field, key)
+        except:
+            pass
+        return result
+
+    def get_bool(self, field, key):
+        result = None
+        try:
+            result = self.cf.getboolean(field, key)
+        except:
+            pass
+        return result
 
 
 if __name__ == "__main__":
+    cf = Config("test.conf")
+    print cf.get("mysql", "user")
+    print cf.get_int("mysql", "port")
+    print cf.get_bool("concur", "switch")
+    print cf.get_float("value", "weight")
 
