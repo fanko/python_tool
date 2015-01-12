08e45a1bf944e5fa9089283923c03207c777f214
rong <rong@rong-qa06-vm.jr.rong360.com>
2015-01-12 10:58:49 +0800

    add myLog.py

--- myLog.py
+++ myLog.py
@@ -3,41 +3,49 @@
 
 
 import sys
+import os
+import time
 import logging
 
 
 # class log
+class Log:
+    def __init__(self, module_name, file_path, min_level):
+        # set the level
+        self.log_level_dict = {"DEBUG":logging.DEBUG, "INFO":logging.INFO, "WARNING":logging.WARNING, "ERROR":logging.ERROR, "CRITICAL":logging.CRITICAL}
 
+        # set the formatter
+        self.formatter = logging.Formatter("[%(levelname)s] [%(asctime)s] [%(filename)s] [%(funcName)s] [%(lineno)d] [%(message)s]")
 
+        # set file name, logger, handler and level for normal log
+        self.log_file_di = os.path.join(file_path, module_name+".log")
+        self.handler_di = logging.FileHandler(self.log_file_di)
+        self.handler_di.setFormatter(self.formatter)
+        self.log_di = logging.getLogger("loggingmodule.NomalLogger")
+        self.log_di.addHandler(self.handler_di)
+        if self.log_level_dict[min_level] > logging.DEBUG:
+            self.log_level_di = self.log_level_dict[min_level]
+        else:
+            self.log_level_di = logging.DEBUG
+        self.log_di.setLevel(self.log_level_di)
+
+        # set file name, logger, handler and level for wf log
+        self.log_file_wf = os.path.join(file_path, module_name+".log.wf")
+        self.handler_wf = logging.FileHandler(self.log_file_wf)
+        self.handler_wf.setFormatter(self.formatter)
+        self.log_wf = logging.getLogger("loggingmodule.wfLogger")
+        self.log_wf.addHandler(self.handler_wf)
+        if self.log_level_dict[min_level] > logging.WARNING:
+            self.log_level_wf = self.log_level_dict[min_level]
+        else:
+            self.log_level_wf = logging.WARNING
+        self.log_wf.setLevel(self.log_level_wf)
 
 
 if __name__ == "__main__":
-    log_file_1 = "./nomal_logger.log"
-    log_file_2 = "./nomal_logger.log.wf"
-    log_level_1 = logging.DEBUG
-    log_level_2 = logging.WARN
-
-    logger_1 = logging.getLogger("loggingmodule.NomalLogger")
-    logger_2 = logging.getLogger("loggingmodule.wfLogger")
-    handler_1 = logging.FileHandler(log_file_1)
-    handler_2 = logging.FileHandler(log_file_2)
-    formatter = logging.Formatter("[%(levelname)s][%(funcName)s][%(asctime)s]%(message)s")
-
-    handler_1.setFormatter(formatter)
-    handler_2.setFormatter(formatter)
-    logger_1.addHandler(handler_1)
-    logger_2.addHandler(handler_2)
-    logger_1.setLevel(log_level_1)
-    logger_2.setLevel(log_level_2)
-
-    logger_1.debug("this is a debug msg!")
-    logger_1.info("this is a info msg!")
-    logger_1.warn("this is a warn msg!")
-    logger_1.error("this is a error msg!")
-    logger_1.critical("this is a critical msg!")
-
-    logger_2.debug("this is a debug msg!")
-    logger_2.info("this is a info msg!")
-    logger_2.warn("this is a warn msg!")
-    logger_2.error("this is a error msg!")
-    logger_2.critical("this is a critical msg!")
+    log = Log("test", ".", "DEBUG")
+    log_di.debug("this is a debug msg!")
+    log_di.info("this is a info msg!")
+    log_wf.warning("this is a warn msg!")
+    log_wf.error("this is a error msg!")
+    log_wf.critical("this is a critical msg!")
